eac25e4b498fd621332335a4b6f900ae5828c4fc
Tadesse Tilahun <ettad2@gmail.com>
2022-07-24 13:54:44 -0600

    added questions to be asked to user

--- app.js
+++ app.js
@@ -11,18 +11,147 @@ const generatePage = require('./src/page-template');
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
-inquirer
+
+
+const promptUser = () => {
+    return inquirer
     .prompt([
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type:'input',
+            name:'github',
+            message: 'Enter your GitHub Username',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }else {
+                    console.log('Please enter your GitHub Username!');
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about your self:'
+        },
+
+    ]);
+};
+
+const promptProject = portfolioData => {
+    console.log(`
+    ==================
+    Add a new Project
+    ==================
+    `);
+
+    // If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                }else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
+
+        },
+
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                }else {
+                    console.log('Please provide a discription of the project!');
+                    return false;
+                }
+            }
+            
+        },
+
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                }else {
+                    console.log('Please enter the GitHub link to your project!');
+                    return false;
+                }
+            }
+        },
+
+
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+
+
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
+
+
     ])
 
-    .then(answers => console.log(answers));
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
+};
 
 
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    console.log(portfolioData);
+});
 
 
 
